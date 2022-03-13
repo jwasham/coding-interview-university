55573a31d5a03b8b04bef35d9ef3df53bb173a71
Ka Yeung <psyky3@nottingham.ac.uk>
2022-03-13 22:47:37 +0000

    Edit array.c for dymamic array

--- c/array.c
+++ c/array.c
@@ -1,45 +1,128 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct
+#include <stddef.h>
+#define N_SIZE 10
+typedef struct Array
 {
     int *array;
     size_t used;
-    size_t size;
+    size_t capacity;
 } Array;
 
-int *sortedSquares(int *nums, int numsSize, int *returnSize);
+void toString(Array *d_arr);
+void init_array(Array *d_arr, size_t n);
+void insertArr(Array *a, int data);
+int find(Array *a, int item);
+int get_capacity(Array *a);
+int get_length(Array *a);
+int find(Array *a, int item);
+int pop(Array *a);
 int main(int argc, char **argv)
 {
+    Array *a;
+    int i = 0;
+    int w;
+    init_array(a, N_SIZE);
+    printf("1: %d", a->capacity);
+
+    for (i = 0; i < 50; i++)
+    {
+        insertArr(a, i);
+    }
+    int *arr;
+    arr = a->array;
+
+    w = pop(a);
+    toString(a);
+    printf("value popped %d\n", w);
+    free(a->array);
     return 0;
 }
 
-int *sortedSquares(int *nums, int numsSize, int *returnSize)
+void init_array(Array *d_arr, size_t n)
 {
-    /*For array that is non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.*/
-    int i = numsSize - 1;
-    int *retarr = (int *)malloc(numsSize * sizeof(int));
-    int *p, *q;
-    p = nums;
-    q = nums + (numsSize - 1);
-    // printf("p: %p, q:%p\n", p, q);
-    while (p <= q)
+    d_arr->array = (int *)malloc(sizeof(int) * n);
+    d_arr->used = 0;
+    d_arr->capacity = n;
+}
+
+void insertArr(Array *a, int data)
+{
+    // check if the capacity is smaller or equal to the size of the array if so we need to expand it.
+    int *arr = a->array;
+    // printf("3");
+    if (a->used >= a->capacity)
     {
-        *p = *p > 0 ? *p : *p * -1;
-        *q = *q > 0 ? *q : *q * -1;
-        printf("*p: %d *q: %d \n", *p, *q);
-        if (*p > *q)
+        printf("Capacity is fulled, expand array is needed\n");
+        a->capacity *= 2;
+
+        if (!(a->array = (int *)realloc(a->array, a->capacity * sizeof(int))))
         {
-            retarr[i--] = *p * (*p);
-            p++;
+            fprintf(stderr, "error: unable to expand the array.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    arr[a->used] = data;
+    a->used++;
+}
+
+void toString(Array *d_arr)
+{
+    int *arr;
+    int i;
+    arr = d_arr->array;
+    printf("[");
+    for (i = 0; i < d_arr->used; i++)
+    {
+        if (i != d_arr->used - 1)
+        {
+            printf("%d,", arr[i]);
         }
         else
         {
-            retarr[i--] = *q * (*q);
-            q--;
+            printf("%d", arr[i]);
         }
     }
+    printf("]");
+}
 
-    return retarr;
+int get_capacity(Array *a)
+{
+    return a->capacity;
+}
+int get_length(Array *a)
+{
+    return a->used;
+}
+void delete (Array *a, int n)
+{
+}
+
+int pop(Array *a)
+{
+    int n;
+    n = a->array[a->used - 1];
+    a->array[--a->used] = 0;
+    return n;
+}
+
+int is_empty(Array *a)
+{
+    if (!a->array)
+        return 0;
+    return 1;
+}
+
+int find(Array *a, int item)
+{
+    int *arr;
+    int i;
+    arr = a->array;
+    for (i = 0; i < a->used; i++)
+    {
+        if (arr[i] == item)
+            return 1;
+    }
+    return 0;
 }
